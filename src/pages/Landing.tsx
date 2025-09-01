c7586ca607ec1f21a627bd42e160ccfad69387bb
Harsh Patel <54549600+harsh-sudo@users.noreply.github.com>
2025-09-01 13:23:10 +0530

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -183,7 +183,7 @@ const Landing = () => {
                             </svg>
                             <div className=" leading-[1.1875rem]">
                                 <p className="m-0">Starts at 12 PM IST on Aug</p>
-                                <p className="m-0">31st Sunday</p>
+                                <p className="m-0">7th Sunday</p>
                             </div>
                         </div>
                     </div>
